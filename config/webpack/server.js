3e6d455504689c1c87554ed8499f0ee15d92a498
andrea-bellini <a.bellini@peoplelab.it>
2019-06-28 11:47:58 +0200

    add https configuration

--- config/webpack/server.js
+++ config/webpack/server.js
@@ -1,5 +1,12 @@
-// const { readFileSync } = require('fs');
+/* eslint-disable no-undef */
+const { readFileSync } = require('fs');
+const path = require('path');
 const { HotModuleReplacementPlugin } = require('webpack');
+
+
+const keyPath = path.resolve(__dirname, '../ssl/localhost/server.key');
+const certPath = path.resolve(__dirname, '../ssl/localhost/server.crt');
+// const caPath = path.resolve(__dirname, '../ssl/localhost/ca.pem');
 
 
 module.exports = {
@@ -10,11 +17,11 @@ module.exports = {
     index: 'index.html',
     historyApiFallback: true,
     hot: true,
-    // https: {
-    //   key: readFileSync('/path/to/server.key'),
-    //   cert: readFileSync('/path/to/server.crt'),
-    //   ca: readFileSync('/path/to/ca.pem'),
-    // },
+    https: {
+      key: readFileSync(keyPath),
+      cert: readFileSync(certPath),
+      // ca: readFileSync(caPath),
+    },
     port: 3000
   },
   plugins: [
