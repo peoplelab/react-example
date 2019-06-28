41c4810b1741ffc1938e1e7a9c90a1db7279f860
andrea-bellini <a.bellini@peoplelab.it>
2019-06-28 11:09:55 +0200

    init https settings on localhost

--- config/webpack/server.js
+++ config/webpack/server.js
@@ -1,3 +1,4 @@
+// const { readFileSync } = require('fs');
 const { HotModuleReplacementPlugin } = require('webpack');
 
 
@@ -9,6 +10,11 @@ module.exports = {
     index: 'index.html',
     historyApiFallback: true,
     hot: true,
+    // https: {
+    //   key: readFileSync('/path/to/server.key'),
+    //   cert: readFileSync('/path/to/server.crt'),
+    //   ca: readFileSync('/path/to/ca.pem'),
+    // },
     port: 3000
   },
   plugins: [
