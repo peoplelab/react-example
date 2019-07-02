09f9fef13e28f982c737f7d7dcd0a0fdc93609e6
andrea-bellini <a.bellini@peoplelab.it>
2019-07-02 10:52:54 +0200

    remove unused imports

--- src/routes/MiddlewareRoute/MiddlewareRouteView.jsx
+++ src/routes/MiddlewareRoute/MiddlewareRouteView.jsx
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import Box from '../../components/layout/Box';
 import Buttons from '../../components/forms/Button';
 
-import '../../style/routes/MiddlewareRoute.scss';
-
 
 class MiddlewareRouteComponent extends Component {
   constructor(props) {
