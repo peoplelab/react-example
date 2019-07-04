83f5df8332831de822143d96b50c094ed7cbaa2d
andrea-bellini <a.bellini@peoplelab.it>
2019-07-04 09:38:42 +0200

    fix redux dev tools implementation

--- src/store/index.js
+++ src/store/index.js
@@ -10,31 +10,24 @@ import sagas from './sagas';
 const { NODE_ENV } = process.env;
 
 const storeConstructor = (initialState = {}) => {
-  let composeEnhancers = compose;
-
   // Redux Dev-Tools
-  const enhancers = [];
-  if (typeof window !== 'undefined' && (NODE_ENV === 'MOCKS' || NODE_ENV === 'DEVELOPMENT')) {
-    // eslint-disable-next-line no-underscore-dangle
-    const composeWithDevToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-
-    if (typeof composeWithDevToolsExtension === 'function') {
-      composeEnhancers = composeWithDevToolsExtension;
-    }
-  }
+  // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers = (
+    typeof window === 'object'
+    && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    && NODE_ENV !== 'PRODUCTION'
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      : compose
+  );
 
   // Saga middleware
   const saga = createSagaMiddleware();
 
+  // Enhancer
+  const enhancer = composeEnhancers(applyMiddleware(saga, ...middlewares));
+
   // Create store
-  const store = createStore(
-    reducers,
-    initialState,
-    composeEnhancers(
-      applyMiddleware(saga, ...middlewares),
-      ...enhancers,
-    ),
-  );
+  const store = createStore(reducers, initialState, enhancer);
 
   // Run sagas
   store.runSaga = saga.run;
