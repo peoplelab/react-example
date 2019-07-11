0cf3505ee185308e9dda53d3f574a3275bb0dac1
andrea-bellini <a.bellini@peoplelab.it>
2019-07-11 10:54:54 +0200

    update mocks handlers

--- mocks/index.js
+++ mocks/index.js
@@ -1,29 +1,33 @@
-const businessPlan = require('./wp-json/wp/v2/business-plan');
+
+const routes = require('./routes');
 
 
+/* eslint-disable no-console */
 module.exports = (app) => {
-  app
-    .route('/wp-json/wp/v2/business-plan/:id')
-    .get(businessPlan.GET);
+  routes.map((route) => {
+    const { url, path } = route;
+    const handler = require(path);
+
+    const appRoute = app.route(url);
+
+    if ('GET' in handler) {
+      appRoute.get(handler.GET);
+    }
+
+    if ('POST' in handler) {
+      appRoute.post(handler.POST);
+    }
+
+    if ('PUT' in handler) {
+      appRoute.put(handler.PUT);
+    }
+
+    if ('PATCH' in handler) {
+      appRoute.patch(handler.PATCH);
+    }
+
+    if ('DELETE' in handler) {
+      appRoute.delete(handler.DELETE);
+    }
+  });
 };
-
-
-// app.get(API_PATH, (req, res) => {
-//   return res.send('Received a GET HTTP method');
-// });
-
-// app.post(API_PATH, (req, res) => {
-//   return res.send('Received a POST HTTP method');
-// });
-
-// app.put(API_PATH, (req, res) => {
-//   return res.send('Received a PUT HTTP method');
-// });
-
-// app.patch(API_PATH, (req, res) => {
-//   return res.send('Received a PATCH HTTP method');
-// });
-
-// app.delete(API_PATH, (req, res) => {
-//   return res.send('Received a DELETE HTTP method');
-// });
--- mocks/wp-json/wp/v2/business-plan/index.js
+++ mocks/wp-json/wp/v2/business-plan/index.js
@@ -6,8 +6,7 @@ module.exports = {
 
     if (id === '50000157910000508014507496221648') {
       status = 200;
-      const path  = `./${id}`;
-      task = require(path);
+      task = require('./50000157910000508014507496221648');
     } else {
       status = 403;
       task = { error: { code: 'EXPIRED', id } };
