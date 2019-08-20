3b5c12213583a5c93e39f4c6cfe12dfcb5bef50b
andrea-bellini <a.bellini@peoplelab.it>
2019-08-20 16:43:15 +0200

    fix file loaders with svg setting

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -19,7 +19,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
             loader: 'file-loader',
